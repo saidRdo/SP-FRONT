d4c8200973ba422ddd2ff9eb6a50b4d88c8211b0
Said Belkaz <saidbelkaz6@gmail.com>
2023-10-02 13:01:42 +0100

    02102023

--- components/locale-switcher.js
+++ components/locale-switcher.js
@@ -2,9 +2,6 @@
 import { usePathname } from 'next/navigation'
 import { useRouter } from 'next/navigation'
 import dynamic from "next/dynamic";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 import {i18n} from "@/i18n.config";
 import Link from "next/link";
 
@@ -47,33 +44,3 @@ function LocaleSwitcher() {
 
 }
 export default dynamic(() => Promise.resolve(LocaleSwitcher), { ssr: false });
-
-/*
- <Autocomplete
-            id="country-select-demo"
-            sx={{ width: 150 ,height:10 }}
-            options={i18n.locales}
-            autoHighlight
-            getOptionLabel={(option) => option.name}
-            renderOption={(props, option) => (
-                <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props} onClick={()=>redirectedPathName(option.url)}>
-                    <img
-                        loading="lazy"
-                        width="20"
-                        src={`${option.avatar}`}
-                        alt=""
-                    />
-                    {option.name}
-                </Box>
-            )}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label={i18n.locales.find(lang=> lang.url==pathName.split('/')[1]).name}
-                    inputProps={{
-                        ...params.inputProps
-                    }}
-                />
-            )}
-        />
- */
